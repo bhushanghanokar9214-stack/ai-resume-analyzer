7fb525211c8ed90edc063a0f727b18a88e76b74b
bhushanghanokar9214-stack <bhushanghanokar9214@gmail.com>
2026-03-14 18:14:41 +0530

    Delete api directory

--- api/analyze.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    score: 75,
-    suggestions: "Add more skills, projects and achievements to improve your resume."
-  });
-}
